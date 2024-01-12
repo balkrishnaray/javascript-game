c35f158b812a0c688e8a0402ad4392b3fd1d3a55
balkrishnray <balkrishnaroy31@gmail.com>
2024-01-12 22:33:00 +0545

    adding random whac

--- Whac-A-Mole/script.js
+++ Whac-A-Mole/script.js
@@ -1,1 +1,30 @@
-let whacValue;
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const score = document.querySelector("#score");
+const timeLeft = document.querySelector("#time-left");
+let result = 0;
+let hitPosition;
+
+function randomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+  let randomSquarebox = squares[Math.floor(Math.random() * 9)];
+  randomSquarebox.classList.add("mole");
+  hitPosition = randomSquarebox.id;
+}
+function moveMole() {
+  let timer = null;
+  timer = setInterval(randomSquare, 500);
+}
+moveMole();
+
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    if (square.id === hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
