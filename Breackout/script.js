991e9eca8405207b4dc16d49018a04ab76626a99
balkrishnray <balkrishnaroy31@gmail.com>
2024-01-15 23:12:09 +0545

    creat block

--- Breackout/script.js
+++ Breackout/script.js
@@ -1,4 +1,22 @@
-const block = document.createElement("div");
-block.classList.add("block");
 const grid = document.querySelector(".grid");
-grid.appendChild(block);
+const blockWidth = 100;
+const blockHeight = 20;
+
+//create block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+function addBlock() {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  grid.appendChild(block);
+  block.style.left = "100px";
+  block.style.bottom = "50px";
+}
+
+addBlock();
